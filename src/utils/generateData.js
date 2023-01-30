77f59f74189c5b92873bef2e3053c1b5e9750de6
Alexander Germoso <mailonywilo@gmail.com>
2023-01-30 15:28:16 -0400

    deploy

--- src/utils/generateData.js
+++ src/utils/generateData.js
@@ -11,7 +11,7 @@ const generateData = async (prompt) => {
             model: "text-davinci-003",
             prompt: prompt,
             temperature: 1,
-            max_tokens: 3800,
+            max_tokens: 3500,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0,
