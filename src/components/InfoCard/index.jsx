ee1a021b793ceb87429c1f44a8fc11faad24bab7
Alexander Germoso <mailonywilo@gmail.com>
2023-01-30 22:55:50 -0400

    style

--- src/components/InfoCard/index.jsx
+++ src/components/InfoCard/index.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const InfoCard = ({ icon = null, title, text, link = null, className }) => {
     return (
         <article
-            className={`flex justify-center flex-col items-start border-black border-solid border rounded-xl py-4 px-8 gap-2 border-opacity-80 ${className}`}
+            className={`w-full flex justify-center flex-col items-start border-black border-solid border rounded-xl py-4 px-8 gap-2 border-opacity-80 ${className}`}
         >
             {icon}
             <div className="flex flex-col gap-1 opacity-80">
